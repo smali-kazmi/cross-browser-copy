93f418cced9f6a6716d66c29dcd0c6a8d48d9fd3
Mudaser Ali <mudasser.ali@careaxiom.com>
2015-05-03 06:10:42 +0500

    Add jcopy.js

---
+++ jcopy.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Auther SM@K <smali.kazmi@hotmail.com>
+ * Website: smak.pk
+ * 
+ */
+
+(function($){
+    $.fn.jCopy = function() {
+      return this.each(function(){
+          
+      });
+    };
+}(jQuery));
+
